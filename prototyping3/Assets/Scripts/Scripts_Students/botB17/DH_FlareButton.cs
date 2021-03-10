a3f210dff811f13c3573efc1ef32daed424bc4d9
DHahnGit <dillonjhahn@gmail.com>
2021-03-10 02:10:40 -0800

    Implementation of Abilities and Traps
    - Ability 1 has a cooldown
    - Ability 2 implemented and can active self-made traps
    - Flare Trap implemented
    - Spike Trap implemented

---
+++ prototyping3/Assets/Scripts/Scripts_Students/botB17/DH_FlareButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DH_FlareButton : MonoBehaviour
+{
+  bool pressed = false;
+  float pressedCooldown = 5.0f;
+  float pressedTimer = 0f;
+
+    // Start is called before the first frame update
+  void Start()
+  {
+        
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if(pressedTimer > 0)
+    {
+      pressedTimer -= Time.deltaTime;
+    }
+    else if(pressed)
+    {
+      pressed = false;
+      transform.GetChild(0).transform.position = new Vector3(transform.GetChild(0).transform.position.x, transform.GetChild(0).transform.position.y + 0.2f, transform.GetChild(0).transform.position.z);
+    }
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    PressButton();
+  }
+
+  public void PressButton()
+  {
+    if (!pressed)
+    {
+      pressed = true;
+      pressedTimer = pressedCooldown;
+      transform.GetChild(0).transform.position = new Vector3(transform.GetChild(0).transform.position.x, transform.GetChild(0).transform.position.y - 0.2f, transform.GetChild(0).transform.position.z);
+      transform.root.GetComponent<DH_FlareTrap>().TurnOnFlare();
+    }
+  }
+}
