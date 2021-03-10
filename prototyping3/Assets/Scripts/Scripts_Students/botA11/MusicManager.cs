e88efd2e899c742bf61088722a95012dc0e4d0bf
AmoghSubhedar <amogh.subhedar@digipen.edu>
2021-03-09 22:29:30 -0800

    Add theme song

---
+++ prototyping3/Assets/Scripts/Scripts_Students/botA11/MusicManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    private AudioSource source;
+
+    private GameHandler handler;
+    private Scene currScene;
+
+    private static bool playedOnce = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        
+        if (playedOnce == false)
+            source.Play();
+
+        playedOnce = true;
+        
+        handler = FindObjectOfType<GameHandler>();
+
+        currScene = SceneManager.GetActiveScene();
+
+        GameHandler.onBattleStart += StopMusicAndDestroy;
+        
+        DontDestroyOnLoad(gameObject);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (source.isPlaying == false)
+            Destroy(gameObject);
+    }
+
+    private void StopMusicAndDestroy()
+    {
+        source.Stop();
+        Destroy(gameObject);
+    }
+    
+}
