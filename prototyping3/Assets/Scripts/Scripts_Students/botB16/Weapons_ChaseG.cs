a44e26c5bcf3487757ab7e0fcf4d29151d7996a0
EmptyGraves42 <76964285+EmptyGraves42@users.noreply.github.com>
2021-03-11 19:59:12 -0800

    Attack and jump are now tied to the same button and there is some feedback on the attack and cooldown.

--- prototyping3/Assets/Scripts/Scripts_Students/botB16/Weapons_ChaseG.cs
+++ prototyping3/Assets/Scripts/Scripts_Students/botB16/Weapons_ChaseG.cs
@@ -8,9 +8,23 @@ public class Weapons_ChaseG : MonoBehaviour
 	// copied BotBasic_Weapon.cs as base for this script
 
 	public GameObject weaponStomp;
-	private float thrustAmount = 1f;
+	private float thrustAmount = 1.0f;
+
+	public Material ReadyMaterial;
+	public Material NotReadyMaterial;
+
+	public GameObject Booster1;
+	public GameObject Booster2;
+	public GameObject Booster3;
+	public GameObject Booster4;
+
+	public float CoolDown;
 
 	private bool weaponOut = false;
+	private bool attack = false;
+	private float lerpItr = 0;
+	private float timer = 0;
+	private float CDtimer;
 
 	//grab axis from parent object
 	public string button1;
@@ -18,25 +32,122 @@ public class Weapons_ChaseG : MonoBehaviour
 	public string button3;
 	public string button4; // currently boost in player move script
 
+	public ParticleSystem BoosterFL_up;
+	public ParticleSystem BoosterBL_up;
+	public ParticleSystem BoosterFR_up;
+	public ParticleSystem BoosterBR_up;
+
+	public ParticleSystem BoosterFL_down;
+	public ParticleSystem BoosterBL_down;
+	public ParticleSystem BoosterFR_down;
+	public ParticleSystem BoosterBR_down;
+
 	void Start()
 	{
 		button1 = gameObject.transform.parent.GetComponent<playerParent>().action1Input;
 		button2 = gameObject.transform.parent.GetComponent<playerParent>().action2Input;
 		button3 = gameObject.transform.parent.GetComponent<playerParent>().action3Input;
 		button4 = gameObject.transform.parent.GetComponent<playerParent>().action4Input;
+
+		CDtimer = CoolDown;
 	}
 
 	void Update()
 	{
 		//if (Input.GetKeyDown(KeyCode.T)){
-		if ((Input.GetButtonDown(button1)) && (weaponOut == false))
+		if ((Input.GetButtonDown(button1)) && (weaponOut == false) && CDtimer >= CoolDown)
 		{
-			Vector3 thrust = new Vector3(0.0f, -10.0f, 0.0f);
+			CDtimer = 0;
+
+			Vector3 thrust = gameObject.GetComponentInParent<Transform>().up * 12;
 			gameObject.GetComponentInParent<Rigidbody>().velocity = thrust;
-			weaponStomp.transform.Translate(0, -thrustAmount, 0);
+
+			//weaponStomp.transform.Translate(0, -thrustAmount, 0);
 			weaponOut = true;
+			attack = true;
 			weaponStomp.tag = "Hazard";
-			StartCoroutine(WithdrawWeapon());
+
+			BoosterFL_down.Play();
+			BoosterBL_down.Play();
+			BoosterFR_down.Play();
+			BoosterBR_down.Play();
+
+			//StartCoroutine(WithdrawWeapon());
+		}
+
+
+		Vector3 startPos = Vector3.zero;
+		Vector3 endPos = new Vector3(0, -thrustAmount, 0);
+
+		if (attack)
+        {
+			if (timer < 0.75f)
+			{
+				timer += Time.deltaTime;
+				BoosterFL_down.Stop();
+				BoosterBL_down.Stop();
+				BoosterFR_down.Stop();
+				BoosterBR_down.Stop();
+			}
+			else
+			{
+
+				weaponStomp.transform.localPosition = (Vector3.Lerp(startPos, endPos, lerpItr));
+				lerpItr += Time.deltaTime * 10;
+				if (lerpItr > 1.0f)
+				{
+					attack = false;
+
+					Vector3 thrust = gameObject.GetComponentInParent<Transform>().up * -17;
+					gameObject.GetComponentInParent<Rigidbody>().velocity = thrust;
+
+					BoosterFL_up.Play();
+					BoosterBL_up.Play();
+					BoosterFR_up.Play();
+					BoosterBR_up.Play();
+
+					Booster1.GetComponent<MeshRenderer>().material = NotReadyMaterial;
+					Booster2.GetComponent<MeshRenderer>().material = NotReadyMaterial;
+					Booster3.GetComponent<MeshRenderer>().material = NotReadyMaterial;
+					Booster4.GetComponent<MeshRenderer>().material = NotReadyMaterial;
+
+					timer = 0;
+				}
+			}
+        }
+		else if (lerpItr > 0.0)
+        {
+			if (timer < 0.5f)
+			{
+				timer += Time.deltaTime;
+			}
+			else
+			{
+				weaponStomp.transform.localPosition = (Vector3.Lerp(startPos, endPos, lerpItr));
+				lerpItr -= Time.deltaTime * 10;
+				
+				weaponStomp.tag = "Untagged";
+				BoosterFL_up.Stop();
+				BoosterBL_up.Stop();
+				BoosterFR_up.Stop();
+				BoosterBR_up.Stop();
+			}
+		}
+		else
+        {
+			timer = 0.0f;
+			weaponStomp.transform.localPosition = Vector3.zero;
+			weaponOut = false;
+		}
+
+		CDtimer += Time.deltaTime;
+
+		if(CDtimer >= CoolDown)
+        {
+			Booster1.GetComponent<MeshRenderer>().material = ReadyMaterial;
+			Booster2.GetComponent<MeshRenderer>().material = ReadyMaterial;
+			Booster3.GetComponent<MeshRenderer>().material = ReadyMaterial;
+			Booster4.GetComponent<MeshRenderer>().material = ReadyMaterial;
 		}
 	}
 
