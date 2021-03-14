efc396d86394ab22e20a58eb1fa049e1af92412a
JasonWiser <JasonWiserArt@gmail.com>
2021-03-14 04:11:25 -0400

    New Camera Follow script has optional rotation follow with bool "alsoFollowRotation"

--- prototyping3/Assets/Scripts/Scripts_Teacher/CameraFollow.cs
+++ prototyping3/Assets/Scripts/Scripts_Teacher/CameraFollow.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
-{
-	public Transform playerObj;
+//this script includes camera move and rotate options. Rotate is turned off by default (alsoFollowRotation = false)
+public class CameraFollow : MonoBehaviour { 
+	[SerializeField] 
+	public Transform playerObj; 
+
 	public bool isPlayer1;
+	public bool alsoFollowRotation = false;
+
+	[SerializeField] 
 	public Vector3 offsetCamera;
 
-	void Start(){
+	[SerializeField] 
+	private Space offsetPositionSpace = Space.Self; 
 
+	[SerializeField] 
+	private bool lookAt = true; 
+
+	void Start(){
 		if (isPlayer1 == true){
 			if (GameObject.FindGameObjectWithTag ("Player1").transform.GetChild(0) != null) {
 				playerObj = GameObject.FindGameObjectWithTag ("Player1").transform.GetChild(0);
@@ -21,12 +31,61 @@ public class CameraFollow : MonoBehaviour
 		}
 	}
 
-	void FixedUpdate () {
+	private void LateUpdate() { 
+		if (alsoFollowRotation == true){
+			MoveAndRotateCamera(); 
+		} else {MoveCamera();}
+	}
+
+	//script for camera to only follow movement, if alsoFollowRotation = false:
+	public void MoveCamera() { 
 		Vector3 pos = Vector3.Lerp ((Vector3)transform.position, (Vector3)playerObj.position + offsetCamera, Time.fixedDeltaTime * 5);
-		//transform.position = new Vector3 (pos.x, pos.y, transform.position.y);
 		transform.position = new Vector3 (pos.x, pos.y, pos.z);
 		transform.LookAt(playerObj);
 	}
 
+	//script for camera to follow movement and rotation, if alsoFollowRotation = true:
+	public void MoveAndRotateCamera() { 
+		Vector3 offsetCameraSide = new Vector3(offsetCamera.x + 1, offsetCamera.y, offsetCamera.z);
+		//if (playerObj == null) { Debug.LogWarning("Missing playerObj ref !", this); return; }
+		// compute position 
+		if (offsetPositionSpace == Space.Self) { transform.position = playerObj.TransformPoint(offsetCameraSide); } 
+		else {transform.position = playerObj.position + offsetCameraSide; }
+		// compute rotation 
+		if (lookAt) { transform.LookAt(playerObj);} 
+		//else {transform.rotation = playerObj.rotation;}
+		else {
+			Transform fromRot = gameObject.transform;
+			Transform toRot = playerObj.transform;
+			float speed = 0.01f;
+			transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, playerObj.transform.rotation, Time.time * speed);
+		}
+	}
 }
 
+
+//the original camera follow script, with just camera move
+// public class CameraFollow : MonoBehaviour
+// {
+	// public Transform playerObj;
+	// public bool isPlayer1;
+	// public Vector3 offsetCamera;
+
+	// void Start(){
+		// if (isPlayer1 == true){
+			// if (GameObject.FindGameObjectWithTag ("Player1").transform.GetChild(0) != null) {
+				// playerObj = GameObject.FindGameObjectWithTag ("Player1").transform.GetChild(0);
+			// }
+		// } else {
+			// if (GameObject.FindGameObjectWithTag ("Player2").transform.GetChild(0) != null) {
+				// playerObj = GameObject.FindGameObjectWithTag ("Player2").transform.GetChild(0);
+			// }
+		// }
+	// }
+	// void FixedUpdate () {
+		// Vector3 pos = Vector3.Lerp ((Vector3)transform.position, (Vector3)playerObj.position + offsetCamera, Time.fixedDeltaTime * 5);
+		// transform.position = new Vector3 (pos.x, pos.y, pos.z);
+		// transform.LookAt(playerObj);
+	// }
+
+// }
