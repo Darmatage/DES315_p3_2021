2d56a043eadbac7938c38b78528e3279d4143b07
Devon Peterson <devonmitchell59@gmail.com>
2021-03-14 23:35:34 -0700

    bot functionality in
    added my bots prototype functionality, gun doesnt work though its having issues

--- prototyping3/Assets/Scripts/Scripts_Students/DevonA13Scripts/Backflip_A13.cs
+++ prototyping3/Assets/Scripts/Scripts_Students/DevonA13Scripts/Backflip_A13.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class Backflip_A13 : MonoBehaviour
 {
-	public float distanceX;
-	public float distanceY;
-	public float cooldown;
+	//flip
+	public float flipdistanceX;
+	public float flipdistanceY;
+	public float flipcooldown;
 	public bool flipping = false;
-	bool cddone = true;
+	//bool cddone = true;
 	float timer = 0.0f;
+
+	//dash
+	public float dashdistance;
+	public float dashcooldown;
+	float dashtimer = 0.0f;
+	float airdashangle = 30.0f;
+	public GameObject lance;
+
+	//bullet
+	public GameObject bullet;
+	public float bulletcooldown = 1.0f;
+	float bullettimer = 0.0f;
 
 	//grab axis from parent object
 	public string button1;
@@ -18,6 +31,7 @@ public class Backflip_A13 : MonoBehaviour
 	public string button4; // currently boost in player move script
 	public string pVertical;
 
+	Rigidbody rb;
 	void Start()
 	{
 		button1 = gameObject.transform.parent.GetComponent<playerParent>().action1Input;
@@ -25,23 +39,79 @@ public class Backflip_A13 : MonoBehaviour
 		button3 = gameObject.transform.parent.GetComponent<playerParent>().action3Input;
 		button4 = gameObject.transform.parent.GetComponent<playerParent>().action4Input;
 		pVertical = gameObject.transform.parent.GetComponent<playerParent>().moveAxis;
-		timer = cooldown;
-		cooldown = 0.0f;
+		timer = flipcooldown;
+		flipcooldown = 0.0f;
+		rb = gameObject.GetComponent<Rigidbody>();
+
+		dashtimer = dashcooldown;
+        dashcooldown = 0.0f;
+
+		bullettimer = bulletcooldown;
+		bulletcooldown = 0.0f;
+
 	}
 
 	// Update is called once per frame
 	void Update()
     {
-		if (cooldown > 0.0f) 
+
+
+		if (flipcooldown > 0.0f)
 		{
-			cooldown -= Time.deltaTime;
+			flipcooldown -= Time.deltaTime;
 		}
 
-		if ((Input.GetButtonDown(button1)) && cooldown <= 0.0f && gameObject.GetComponent<BotBasic_Move>().isGrounded && !gameObject.GetComponent<BotBasic_Move>().isGrabbed) 
+		if (dashcooldown > 0.0f)
 		{
-			gameObject.GetComponent<Rigidbody>().AddForce(-Input.GetAxisRaw(pVertical) * new Vector3(distanceX, distanceY, 0));
-			cooldown = timer;
-			//flipping = true;
+			dashcooldown -= Time.deltaTime;
+		}
+
+		if (bulletcooldown > 0.0f) 
+		{
+			bulletcooldown -= Time.deltaTime;
+		}
+
+		if ((Input.GetButtonDown(button1)) && bulletcooldown <= 0.0f && !gameObject.GetComponent<BotBasic_Move>().isGrabbed) 
+		{
+			GameObject bull = Instantiate(bullet, transform.position, transform.rotation);
+			bull.GetComponent<A13_Bullet>().playertag = gameObject.tag;
+			bulletcooldown = bullettimer;
+		}
+
+
+		if ((Input.GetButtonDown(button2)) && dashcooldown <= 0.0f && !gameObject.GetComponent<BotBasic_Move>().isGrabbed) 
+		{
+			if (!gameObject.GetComponent<BotBasic_Move>().isGrounded)
+			{
+				
+				transform.Rotate(new Vector3(transform.rotation.x + airdashangle, transform.rotation.y, transform.rotation.z), Space.Self);
+			}
+			rb.velocity = Vector3.zero;
+			rb.AddForce(rb.centerOfMass + gameObject.transform.forward * dashdistance, ForceMode.Impulse);
+			dashcooldown = dashtimer;
+
+			//attack
+			lance.GetComponent<MeshCollider>().enabled = true;
+			StartCoroutine(activatelance());
+			gameObject.GetComponent<AudioSource>().Play();
+		}
+
+		if ((Input.GetButtonDown(button3)) && flipcooldown <= 0.0f && gameObject.GetComponent<BotBasic_Move>().isGrounded && !gameObject.GetComponent<BotBasic_Move>().isGrabbed) 
+		{
+			rb.AddForce(rb.centerOfMass + (new Vector3(gameObject.transform.forward.x * -flipdistanceX, flipdistanceY, gameObject.transform.forward.z * -flipdistanceX)), ForceMode.Impulse);
+			flipcooldown = timer;
 		}
     }
+
+	IEnumerator activatelance()
+	{
+		yield return new WaitForSeconds(0.3f);
+		lance.GetComponent<MeshCollider>().enabled = false;
+	}
+
+	IEnumerator backflipanim() 
+	{
+		yield return new WaitForSeconds(1.0f);
+
+	}
 }
