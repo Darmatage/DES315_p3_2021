ab2f5f41d06afd6a3e73455299ecc5df2e1e7d7e
aloranewbury <aloranewbury@users.noreply.github.com>
2021-03-15 10:01:07 -0700

    Bot AO5

---
+++ prototyping3/Assets/Scripts/Scripts_Students/AloraNewbury/Teleport.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+
+    public GameObject player;
+    public AudioSource Begin;
+    public AudioSource End;
+    public ParticleSystem particle;
+    public bool is_Teleporting;
+    public float cooldown;
+
+
+    Vector3 GetBehindlocation(float distance_behind)
+    {
+        Transform child = GameObject.FindGameObjectWithTag("Player2").transform.GetChild(0);
+
+        return child.transform.position - (child.transform.forward * distance_behind);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cooldown = 0.0f;
+        is_Teleporting = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown <= 0.0f)
+        {
+            is_Teleporting = true;
+            Begin.Play();
+            particle.Play();
+
+        }
+        if (is_Teleporting == true && Begin.isPlaying == false)
+        {
+            player.transform.position = GetBehindlocation(5.0f);
+
+            End.Play();
+
+            cooldown = 5.0f;
+
+            is_Teleporting = false;
+        }
+        if (cooldown > 0.0f)
+        {
+            cooldown -= 0.1f;
+        }
+
+    }
+}
