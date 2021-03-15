f6be594bae79e508c3a8398ea799f90690965de2
AdamDoo <35050357+AdamDoo@users.noreply.github.com>
2021-03-14 19:00:34 -0700

    Made Progress on Jetpack

--- prototyping3/Assets/Scripts/Scripts_Students/AdamDoolittle/Jetpack.cs
+++ prototyping3/Assets/Scripts/Scripts_Students/AdamDoolittle/Jetpack.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Jetpack : MonoBehaviour
 {
-    float speed = 6.0f;
-    float gravity = 20.0f;
+    public string button1;
+    public string button2;
+    public string button3;
+    public string button4;
 
-    Vector3 moveDirection = Vector3.zero;
+    bool isFacingUp = false;
 
-    ConstantForce force;
+    float fuel = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        button1 = gameObject.transform.parent.GetComponent<playerParent>().action1Input;
+        button2 = gameObject.transform.parent.GetComponent<playerParent>().action2Input;
+        button3 = gameObject.transform.parent.GetComponent<playerParent>().action3Input;
+        button4 = gameObject.transform.parent.GetComponent<playerParent>().action4Input;
     }
 
     // Update is called once per frame
@@ -23,12 +28,29 @@ public class Jetpack : MonoBehaviour
         var botController = GetComponent<BotBasic_Move>();
         var rb = GetComponent<Rigidbody>();
 
-        if(Input.GetKey(KeyCode.R))
+        if(Input.GetButton(button2))
         {
-            //moveDirection.y = botController.jumpSpeed;
-            rb.AddForce(rb.centerOfMass + new Vector3(0f, botController.jumpSpeed * 10, 0f), ForceMode.Force);
-            //rb.AddForce(rb.centerOfMass + new Vector3(0f, force.relativeForce.y * 10, 0f));
+            if (fuel != 0.0f)
+            {
+                rb.AddForce(rb.centerOfMass + new Vector3(0f, botController.jumpSpeed * 10, 0f), ForceMode.Force);
+                fuel -= Time.deltaTime;
+                Debug.Log(fuel);
+                if (isFacingUp == false)
+                {
+                    transform.Rotate(-90, 0, 0);
+                    isFacingUp = true;
+                }
+            }
         }
-
+        if(Input.GetButtonUp(button2))
+        {
+            isFacingUp = false;
+            transform.Rotate(90, 0, 0);
+            if(fuel == 0.0f && fuel != 2.0f)
+            {
+                fuel += Time.deltaTime;
+            }
+        }
+        
     }
 }
