06d52f6e513334b3dd1820dcc435d7a75539732e
Austin Stead <austin.s@digipen.edu>
2021-03-15 08:44:32 -0700

    Shield Weapons
    Added first version of guns on the tank.

---
+++ prototyping3/Assets/Scripts/Scripts_Students/botB04/BotB04_Gun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BotB04.Controller
+{
+    public class BotB04_Gun : MonoBehaviour
+    {
+        public GameObject bullet;
+        public GameObject bulletSpawn;
+
+
+        public void Fire()
+        {
+            GameObject.Instantiate(bullet, bulletSpawn.transform);
+            bullet.transform.parent = null;
+        }
+
+
+
+    }
+}
---
+++ prototyping3/Assets/Scripts/Scripts_Students/botB04/BotB04_ShieldWeapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace BotB04.Controller
+{
+    [System.Serializable]
+    public class BotB04_ShieldWeapon
+    {
+        [SerializeField]
+        private GameObject weaponRef;
+        [HideInInspector]
+        public BotB04_Gun weapon;
+        public RechargableShield shieldStats;
+
+        public void Initialize()
+        {
+            weapon = weaponRef.GetComponent<BotB04_Gun>();
+        }
+
+        public void RequestFire()
+        {
+            //If shields are recharging, can't fire
+            if(shieldStats.recharging)
+            {
+                return;
+            }
+
+            weapon.Fire();
+        }
+
+    }
+}
