d14db2f196a67d3c2ecd6ec3a3dc0e3367498eb1
Dieter Voegels <dietervoegels@gmail.com>
2021-03-15 19:29:14 -0700

    First Draft
    - Added Blades to the each side
    - Added a "Charge" which is activated by M

---
+++ prototyping3/Assets/Scripts/Scripts_Students/DieterVoegels/Charge.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charge : MonoBehaviour
+{
+  public GameObject leftPlow;
+  public GameObject rightPlow;
+  public float damage = 2.0f;
+  public float chargeCooldown = 2.0f;
+  public float chargeSpeed = 10.0f;
+
+  bool charge = false;
+  float chargeTimer = 0.0f;
+  float damageTimer = 0.0f;
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.M) == true && chargeTimer <= 0)
+    {
+      charge = true;
+    }
+
+    if (charge == true)
+    {
+      gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * chargeSpeed, ForceMode.Impulse);
+      leftPlow.GetComponent<HazardDamage>().damage = damage;
+      rightPlow.GetComponent<HazardDamage>().damage = damage;
+      charge = false;
+      chargeTimer = chargeCooldown;
+      damageTimer = .25f;
+    }
+
+    if (chargeTimer > 0)
+    {
+      chargeTimer -= Time.deltaTime;
+    }
+
+    if (damageTimer > 0)
+    {
+      damageTimer -= Time.deltaTime;
+    }
+    else
+    {
+      leftPlow.GetComponent<HazardDamage>().damage = 0;
+      rightPlow.GetComponent<HazardDamage>().damage = 0;
+    }
+  }
+}
