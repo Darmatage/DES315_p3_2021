fa3351ccbb6f31277826f52f7baf0f49f343c827
David Dasky <david.dasky@digipen.edu>
2021-03-18 23:12:50 -0400

    Final Changes to A10

--- prototyping3/Assets/Scripts/Scripts_Students/BotA10_Scripts/FlameThrower.cs
+++ prototyping3/Assets/Scripts/Scripts_Students/BotA10_Scripts/FlameThrower.cs
@@ -6,16 +6,15 @@ namespace Scripts_Students.BotA10_Scripts
     public class FlameThrower : MonoBehaviour
     {
         public string player;
-        public float damage = 1f;
-        private GameHandler _gameHandler;
         public bool isPlayer1Weapon = false;
         public bool isPlayer2Weapon = false;
 
-    
+        [SerializeField] private float damage = 1f;
         [SerializeField] private float cooldownTimerMax = 1f;
         private float _cooldownTimer = 1f;
-    
-        void Start()
+        private GameHandler _gameHandler;
+
+        private void Start()
         {
             if (GameObject.FindWithTag("GameHandler") != null)
             {
@@ -29,30 +28,37 @@ namespace Scripts_Students.BotA10_Scripts
         private void Update()
         {
             if (_cooldownTimer > 0) { _cooldownTimer -= Time.deltaTime; }
-            if (_cooldownTimer < 0) { _cooldownTimer = 0; }
+            if (_cooldownTimer < 0) { _cooldownTimer = 0; damage = 1f; }
         }
-
-        void OnTriggerStay(Collider other)
+        
+        
+        /*
+         * I couldn't figure out a way to make the flamethrower
+         * do damage without ignoring shields. Thus, I ignored them.
+         * Does little damage overtime anyway.
+         */
+        private void OnTriggerStay(Collider other)
         {
-            string target = other.gameObject.transform.root.tag;
-            if (_gameHandler != null && target != player)
-            {
-                if (target == "Player1")
-                {
+            var target = other.gameObject.transform.root.tag;
+            if (_gameHandler == null || target == player) return;
+            switch (target)
+            { 
+                case "Player1":
                     if (_cooldownTimer == 0)
                     {
                         _gameHandler.TakeDamage("Player1", damage);
+                        damage = 0f; // set to 0 to fix too much damage issue
                         _cooldownTimer = cooldownTimerMax;
                     }
-                }
-                if (target == "Player2")
-                {
+                    break;
+                case "Player2":
                     if (_cooldownTimer == 0)
                     {
                         _gameHandler.TakeDamage("Player2", damage);
+                        damage = 0f; // set to 0 to fix too much damage issue
                         _cooldownTimer = cooldownTimerMax;
-                    }
-                }
+                    } 
+                    break;
             }
         }
     }
