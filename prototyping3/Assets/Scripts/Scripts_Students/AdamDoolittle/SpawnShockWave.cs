5718a3c720431fdfbd59c42308a5eddc5e70f3cc
AdamDoo <35050357+AdamDoo@users.noreply.github.com>
2021-03-22 17:51:58 -0700

    Made more progress on shockwave

--- prototyping3/Assets/Scripts/Scripts_Students/AdamDoolittle/SpawnShockWave.cs
+++ prototyping3/Assets/Scripts/Scripts_Students/AdamDoolittle/SpawnShockWave.cs
@@ -5,30 +5,49 @@ using UnityEngine;
 public class SpawnShockWave : MonoBehaviour
 {
     public GameObject shockwave;
+    public GameObject newShock;
+    //Vector3 shockwaveEndPos;
+    //Vector3 shockwaveStartPos;
 
-    Vector3 shockwaveEndPos;
-    Vector3 shockwaveStartPos;
+    public Vector3 targetScale;
 
-    float aliveTimer = 3.0f;
+    float aliveTimer = 5.0f;
+    public float speed = 2f;
 
-    BotBasic_Move botController;
+    public bool canGrow = false;
+
+    public BotBasic_Move botController;
 
     // Start is called before the first frame update
     void Start()
     {
-        shockwaveEndPos = new Vector3(shockwave.transform.position.x + 100, shockwave.transform.position.y, shockwave.transform.position.z + 100);
-        shockwaveStartPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        //shockwaveEndPos = new Vector3(shockwave.transform.position.x + 100, shockwave.transform.position.y, shockwave.transform.position.z + 100);
+        //shockwaveStartPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        targetScale = new Vector3(60f, 0.005f, 60f);
+        botController = transform.parent.GetComponent<BotBasic_Move>();
+    }
+
+    private void Update()
+    {
+        if(canGrow == true)
+        {
+            newShock.transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * speed);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //var botController = GetComponent<BotBasic_Move>();
 
-        if(botController.isGrounded == true && aliveTimer <= 3.0f)
+        if((botController.isGrounded == true) && (aliveTimer <= 3.0f))
         {
             aliveTimer += Time.deltaTime;
             Debug.Log(aliveTimer);
+            if(aliveTimer <= 0)
+            {
+                aliveTimer = 0;
+            }
         }
     }
 
@@ -36,13 +55,24 @@ public class SpawnShockWave : MonoBehaviour
     {
         if (other.gameObject.layer == 8)
         {
-            Instantiate(shockwave, shockwaveStartPos, Quaternion.identity);
-            Vector3.Lerp(shockwaveStartPos, shockwaveEndPos, 2.0f);
-            while (aliveTimer <= 3.0f && aliveTimer != 0.0f)
-            {
-                aliveTimer -= Time.deltaTime;
-                Debug.Log(aliveTimer);
-            }
+            Debug.Log("I hit the ground!");
+            newShock = Instantiate(shockwave, transform.position, Quaternion.identity);
+            //newShock.transform.localScale = Vector3.Lerp(shockwaveStartPos, shockwaveEndPos, 2.0f);
+            canGrow = true;
+            StartCoroutine(DestroyShock(newShock));
+            //while (aliveTimer <= 3.0f && aliveTimer != 0.0f)
+            //{
+            //    aliveTimer -= Time.deltaTime;
+            //    Debug.Log(aliveTimer);
+            //}
+            //Destroy(newShock);
         }
+    }
+
+    IEnumerator DestroyShock(GameObject thisShock)
+    {
+        yield return new WaitForSeconds(aliveTimer);
+        canGrow = false;
+        Destroy(thisShock);
     }
 }
