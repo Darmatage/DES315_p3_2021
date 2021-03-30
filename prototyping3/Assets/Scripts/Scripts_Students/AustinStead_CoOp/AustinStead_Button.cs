7032bee6a3f8288ccbf314242036908e7e0293de
Austin Stead <austin.s@digipen.edu>
2021-03-30 01:19:30 -0700

    Initial level, button door.

---
+++ prototyping3/Assets/Scripts/Scripts_Students/AustinStead_CoOp/AustinStead_Button.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AustinStead_Button : MonoBehaviour
+{
+    public float CooldownDuration = 1.0f;
+
+    private float cooldownClock;
+
+    public delegate void ButtonAction();
+    public event ButtonAction ButtonActivated;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cooldownClock = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownClock = Mathf.Max(0, cooldownClock - Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if(cooldownClock <= 0)
+        {
+            //cooldownClock = CooldownDuration;
+
+            if (ButtonActivated != null)
+                ButtonActivated();
+        }
+
+    }
+
+
+
+
+
+}
---
+++ prototyping3/Assets/Scripts/Scripts_Students/AustinStead_CoOp/AustinStead_SlidingDoor.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AustinStead
+{
+
+    public class AustinStead_SlidingDoor : MonoBehaviour
+    {
+        public bool IsOpen = false;
+
+        public AustinStead_Button Button;
+
+        public GameObject door;
+
+        private Animator doorAnim;
+
+        public Material OpenMat;
+        public Material ClosedMat;
+
+
+        private Renderer renderer;
+
+        private void OnEnable()
+        {
+            Button.ButtonActivated += Activate;
+        }
+        private void OnDisable()
+        {
+            Button.ButtonActivated -= Activate;
+        }
+
+        private void Start()
+        {
+            doorAnim = door.GetComponent<Animator>();
+            renderer = door.GetComponent<Renderer>();
+
+            doorAnim.SetBool("IsOpen", IsOpen);
+        }
+
+        void Activate()
+        {
+
+            IsOpen = !IsOpen;
+            doorAnim.SetBool("IsOpen", IsOpen);
+
+            Debug.Log("IsOpen = " + IsOpen);
+
+            if (IsOpen)
+            {
+                renderer.material = OpenMat;
+            }
+            else
+            {
+                renderer.material = ClosedMat;
+            }
+
+        }
+
+
+    }
+}
+
