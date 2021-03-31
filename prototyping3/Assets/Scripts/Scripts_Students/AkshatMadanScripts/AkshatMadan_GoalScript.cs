285a86c39a7b5b9d448b7f0513753f1928eb4d0f
Akshat Madan <akshatmadan135@gmail.com>
2021-03-31 10:24:29 -0700

    Added goals and bot selector

---
+++ prototyping3/Assets/Scripts/Scripts_Students/AkshatMadanScripts/AkshatMadan_GoalScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AkshatMadan_GoalScript : MonoBehaviour
+{
+    public bool isGoal2 = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Ball")
+        {
+            // Goal scored
+            if (isGoal2)
+                Debug.Log("P1 scored");
+            else
+                Debug.Log("P2 scored");
+        }
+    }
+}
