bbc851fd9cdc5ad2c5b83c571cc4f9be2a996292
wmielke <willmielke@comcast.net>
2021-04-05 08:25:29 -0600

    added spike trap base and got wall working

--- prototyping3/Assets/Scripts/Scripts_Students/WillMielke/BotB09_WallButton.cs
+++ prototyping3/Assets/Scripts/Scripts_Students/WillMielke/BotB09_WallButton.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class BotB09_WallButton : MonoBehaviour
 {
+    public bool move;
+    public Material offMat;
+    public Material onMat;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        move = false;
     }
 
     // Update is called once per frame
@@ -15,4 +19,18 @@ public class BotB09_WallButton : MonoBehaviour
     {
         
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        move = !move;
+        if(move)
+        {
+            gameObject.GetComponent<Renderer>().material = onMat;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material = offMat;
+        }
+    }
+
 }
