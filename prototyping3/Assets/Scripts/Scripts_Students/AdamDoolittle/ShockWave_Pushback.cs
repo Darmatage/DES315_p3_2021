44f3a6b07dfaf7c5ef6a5af81f2925dd8fc4a772
AdamDoo <35050357+AdamDoo@users.noreply.github.com>
2021-04-05 18:14:35 -0700

    Made Progress on Shockwave pushback

---
+++ prototyping3/Assets/Scripts/Scripts_Students/AdamDoolittle/ShockWave_Pushback.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockWave_Pushback : MonoBehaviour
+{
+    public Transform source;
+    public float pushBack = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Vector3 dir = new Vector3(other.transform.position.x - source.position.x, 0, other.transform.position.z - source.position.z).normalized;
+        dir += Vector3.up * 0.5f;
+
+        Rigidbody rb = other.transform.root.GetChild(0).GetComponent<Rigidbody>();
+        rb.AddForce(dir * pushBack, ForceMode.Impulse);
+
+        transform.GetComponent<Collider>().enabled = false;
+
+    }
+}
