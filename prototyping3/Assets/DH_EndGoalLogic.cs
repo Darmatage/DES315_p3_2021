240a49f7b2e8de56f7de51b48a04934e6c4e55f7
DHahnGit <dillonjhahn@gmail.com>
2021-04-07 09:04:41 -0700

    Fixed Mechanics

---
+++ prototyping3/Assets/DH_EndGoalLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DH_EndGoalLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if ((other.transform.root.gameObject.tag == "Player1" || other.transform.root.gameObject.tag == "Player2"))
+    {
+      GameObject.Find("GameHandler").GetComponent<GameHandler>().coopPlayersWin = true;
+      StartCoroutine(GameObject.Find("GameHandler").GetComponent<GameHandler>().CoopEndGame());
+    }
+  }
+}
