235a8918f0410e8458acf81aa1e7b390c61ad1f2
aloranewbury <aloranewbury@gmail.com>
2021-04-10 17:53:37 -0700

    Additions to Look and functionality

--- prototyping3/Assets/Scripts/Scripts_Students/AloraNewbury/OpenGate.cs
+++ prototyping3/Assets/Scripts/Scripts_Students/AloraNewbury/OpenGate.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class OpenGate : MonoBehaviour
 {
+
+    public AudioSource open;
+
+    public ButtonPress button;
+    public bool is_open;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        is_open = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (is_open == true && open.isPlaying == false)
+        {
+            Destroy(gameObject);
+
+        }
+        if (button.gate_opened == true && is_open == false)
+        {
+            open.Play();
+            is_open = true;
+        }
+      
     }
 }
