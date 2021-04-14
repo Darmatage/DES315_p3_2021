ae2b915e0de4e26d3588c14f42e94bc0d957f3b7
DHahnGit <dillonjhahn@gmail.com>
2021-04-14 01:28:57 -0700

    QoL features to tie up the level

---
+++ prototyping3/Assets/Scripts/Scripts_Students/DillonHahn/DH_ArrowLogic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DH_ArrowLogic : MonoBehaviour
+{
+
+  public float rotationSpeed = 5;
+  public float bobSpeed = 5;
+  public float bobIntensity = 2;
+
+  Vector3 startPos;
+
+  float timer = 0f;
+  float currentRotation = 0f;
+  
+
+  void Start()
+  {
+    startPos = transform.position;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    timer += Time.deltaTime * bobSpeed;
+
+    transform.position = startPos + new Vector3(0, Mathf.Sin(timer) * bobIntensity, 0);
+
+    currentRotation += Time.deltaTime * rotationSpeed;
+
+    transform.rotation = Quaternion.Euler(0, currentRotation, 0);
+  }
+}
