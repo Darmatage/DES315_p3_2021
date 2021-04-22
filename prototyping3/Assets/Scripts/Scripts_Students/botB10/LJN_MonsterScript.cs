435953dec04e7632303d9257a65040301ff1876b
Lowell Novitch <minimooch@icloud.com>
2021-04-22 00:05:49 -0700

    2 attack patterns

--- prototyping3/Assets/Scripts/Scripts_Students/botB10/LJN_MonsterScript.cs
+++ prototyping3/Assets/Scripts/Scripts_Students/botB10/LJN_MonsterScript.cs
@@ -52,6 +52,11 @@ public class LJN_MonsterScript : MonoBehaviour
     private float spinTimer;
 
     private int attackChoice;
+    private int attackStage = 200;
+
+    public Vector3 EndRot;
+
+    private Vector3 currentEnd ;
  
 
     // Start is called before the first frame update
@@ -60,6 +65,7 @@ public class LJN_MonsterScript : MonoBehaviour
         myAgent = GetComponent<NavMeshAgent>();
         playerLoader = GetComponent<NPC_LoadPlayers>();
         nextPatrolTarget = defaultPatrolTarget;
+        currentEnd = SawArmEnd;
     }
 
     // Update is called once per frame
@@ -75,55 +81,77 @@ public class LJN_MonsterScript : MonoBehaviour
             distToPlayer2 = Vector3.Distance(player2Target.position, gameObject.transform.position);
 
             //is the player close enough to attack?
-            if ((distToPlayer1 <= playerAttackDistance) && (distToPlayer1 < distToPlayer2))
-            {
-                attackPlayer1 = true;
-            }
-            else
-            {
-                attackPlayer1 = false;
-            }
+            //if ((distToPlayer1 <= playerAttackDistance) && (distToPlayer1 < distToPlayer2))
+            //{
+            //    attackPlayer1 = true;
+            //}
+            //else
+            //{
+            //    attackPlayer1 = false;
+            //}
 
-            if ((distToPlayer2 <= playerAttackDistance) && (distToPlayer2 < distToPlayer1))
-            {
-                attackPlayer2 = true;
-            }
-            else
-            {
-                attackPlayer2 = false;
-            }
+            //if ((distToPlayer2 <= playerAttackDistance) && (distToPlayer2 < distToPlayer1))
+            //{
+            //    attackPlayer2 = true;
+            //}
+            //else
+            //{
+            //    attackPlayer2 = false;
+            //}
         }
 
         //am I moving towards the player or my next patrol location? 
-        if (attackPlayer1 == true)
+        //if (attackPlayer1 == true)
+        //{
+        //    myAgent.destination = player1Target.position;
+        //    if (distToPlayer1 > turnThreshold && attackChoice == 0 && attackStage ==0)
+        //    {
+        //        transform.LookAt(player1Target);
+        //    }
+        //    isAttacking = true;
+        //    AttackPlayer();
+        //}
+        //else if (attackPlayer2 == true)
+        //{
+        //    myAgent.destination = player2Target.position;
+        //    if (distToPlayer2 > turnThreshold && attackChoice == 0 && attackStage == 0)
+        //    {
+        //        transform.LookAt(player2Target);
+        //    }
+        //    isAttacking = true;
+        //    AttackPlayer();
+        //}
+        //else
+        if(distToTarget <= playerAttackDistance && attackStage !=200)
         {
-            myAgent.destination = player1Target.position;
-            if (distToPlayer1 > turnThreshold)
+           // myAgent.destination = nextPatrolTarget.position;
+            if (attackChoice == 0)
             {
-                transform.LookAt(player1Target);
+                if (attackStage == 0)
+                {
+                    if (distToTarget > turnThreshold )
+                    {
+                        transform.LookAt(nextPatrolTarget);
+                    }
+                    attackStage = 1;
+                }
+                isAttacking = true;
+
             }
-            isAttacking = true;
+            else if (attackChoice == 1)
+            {
+                if (attackStage == 0)
+                {
+                    attackStage = 1;
+                    isAttacking = true;
+                    myAgent.destination = nextPatrolTarget.position;
+                }
+            }
             AttackPlayer();
         }
-        else if (attackPlayer2 == true)
-        {
-            myAgent.destination = player2Target.position;
-            if (distToPlayer2 > turnThreshold)
-            {
-                transform.LookAt(player2Target);
-            }
-            isAttacking = true;
-            AttackPlayer();
-        }
-        else
+        else if(distToTarget > playerAttackDistance)
         {
             myAgent.destination = nextPatrolTarget.position;
-            if (distToTarget > turnThreshold)
-            {
-                transform.LookAt(nextPatrolTarget);
-            }
-            isAttacking = false;
-            AttackPlayer();
         }
 
         //if (distToTarget <= patrolSwitchThreshold1)
@@ -144,7 +172,7 @@ public class LJN_MonsterScript : MonoBehaviour
         {
             LoadPlayerTargets();
 
-            if (distToTarget <= playerAttackDistance)
+            if (attackStage == 200)//distToTarget <= playerAttackDistance
             {
                 ChooseTarget();
                
@@ -159,7 +187,7 @@ public class LJN_MonsterScript : MonoBehaviour
 
             if (SawLerpAmount < 0.0f) SawLerpAmount = 0.0f;
 
-            tail.transform.localEulerAngles = Vector3.Lerp(SawArmStart, SawArmEnd, SawLerpAmount);
+            tail.transform.localEulerAngles = Vector3.Lerp(SawArmStart, currentEnd, SawLerpAmount);
         }
     }
 
@@ -168,11 +196,58 @@ public class LJN_MonsterScript : MonoBehaviour
         float dt = Time.deltaTime;
         //use attack pattern
 
-        SawLerpAmount += dt * SawSpeed;
+        if (attackChoice == 0 && attackStage == 1)
+        {
+            SawLerpAmount += dt * SawSpeed;
 
-        if (SawLerpAmount > 1.0f) SawLerpAmount = 1.0f;
-        tail.transform.localEulerAngles = Vector3.Lerp(SawArmStart, SawArmEnd, SawLerpAmount);
+            if (SawLerpAmount > 1.0f) SawLerpAmount = 1.0f;
+            tail.transform.localEulerAngles = Vector3.Lerp(SawArmStart, SawArmEnd, SawLerpAmount);
 
+            if(attackTimer >= 1.5f)
+            {
+                attackStage = 200;
+                isAttacking = false;
+            }
+        }
+
+        if(attackChoice == 1)
+        {
+
+            if (attackStage != 2)
+            {
+                SawLerpAmount += dt * SawSpeed;
+
+                if (SawLerpAmount > 1.0f)
+                    SawLerpAmount = 1.0f;
+                tail.transform.localEulerAngles = Vector3.Lerp(SawArmStart, EndRot, SawLerpAmount);
+            }
+            else
+            {
+                if (SawLerpAmount == 0.0f)
+                {
+                    attackStage = 200;
+                }
+            }
+
+            if (SawLerpAmount == 1.0f && attackStage == 0)
+            {
+                attackStage = 1;
+                attackTimer = 0;
+            }
+
+            if(attackStage == 1)
+            {
+                transform.Rotate(new Vector3(0, 420 * dt,0));
+               // Debug.Log(tail.GetComponent<HazardDamage>().damage);
+
+                if(attackTimer >= 4)
+                {
+                    attackStage = 2;
+                    isAttacking = false;
+                }
+            }
+
+        }
 
         attackTimer += dt;
     }
@@ -227,16 +302,18 @@ public class LJN_MonsterScript : MonoBehaviour
     private void ChooseTarget()
     {
         //based on attack pattern
-        if(attackTimer >= 2.5f)
+        if(attackStage == 200)
         {
+            attackStage = 0;
             if(attackChoice == 1 )
             {
                 tail.GetComponent<HazardDamage>().damage = 0;
             }
 
-            attackChoice = Random.Range(0, 3);
-
-            switch(attackChoice)
+             attackChoice = Random.Range(0, 2);
+            
+            tail.transform.localPosition = new Vector3(0f, 0.5f, -2.01f);
+            switch (attackChoice)
             {
                 case 0: //charge
                     if(Random.Range(1,3) == 1)
@@ -247,9 +324,12 @@ public class LJN_MonsterScript : MonoBehaviour
                     {
                         nextPatrolTarget = player2Target;
                     }
+                    currentEnd = SawArmEnd;
                     break;
                 case 1: //spin
                     tail.GetComponent<HazardDamage>().damage = 10;
+                    tail.transform.localPosition = new Vector3(0, 0, -2.01f);
+                    currentEnd = EndRot;
                     break;
                 case 2: //block and push
                     break;
